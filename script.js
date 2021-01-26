800dd0d8c3f31f3dd946b2049926800eee30b785
Nurkyz-Chynybaeva <nurkyzchynybaeva29@gmail.com>
2021-01-26 21:52:59 +0600

    added multiplication 2

---
+++ script.js
@@ -0,0 +1,10 @@
+
+
+for (let i = 1; i <= 10; i++){
+  for (let i2 = 2; i2 <= 2; i2++)
+  {
+   
+   document.write("<table border=3 >" + "<tr>" +"<td>" + i2 + " * " + i + " = " + i2 * i + "|||||||||||||" + "</td>" + "</tr>" + "</table" );
+  }
+  document.write("<br");
+}
