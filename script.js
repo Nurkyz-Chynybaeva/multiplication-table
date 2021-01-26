c437ac003b7dad734ea3227d78c2bb476262cd05
Nurkyz-Chynybaeva <nurkyzchynybaeva29@gmail.com>
2021-01-26 21:59:17 +0600

    multiplication 3 and 4

--- script.js
+++ script.js
@@ -4,7 +4,31 @@ for (let i = 1; i <= 10; i++){
   for (let i2 = 2; i2 <= 2; i2++)
   {
    
-   document.write("<table border=3 >" + "<tr>" +"<td>" + i2 + " * " + i + " = " + i2 * i + "|||||||||||||" + "</td>" + "</tr>" + "</table" );
+   document.write("<table border=3 >" + "<tr>" +"<td>" + i2 + " * " + i + " = " + i2 * i + "</td>" + "</tr>" + "</table" );
   }
   document.write("<br");
 }
+
+
+
+for (let i = 1; i <= 10; i++){
+  for (let i2 = 3; i2 <= 3; i2++)
+  {
+   
+   document.write("<table border=3 >" + "<tr>" +"<td>" + i2 + " * " + i + " = " + i2 * i + "</td>" + "</tr>" + "</table" );
+  }
+  document.write("<br");
+}
+
+
+
+for (let i = 1; i <= 10; i++){
+  for (let i2 = 4; i2 <= 4; i2++)
+  {
+   
+   document.write("<table border=3 >" + "<tr>" +"<td>" + i2 + " * " + i + " = " + i2 * i + "</td>" + "</tr>" + "</table" );
+  }
+  document.write("<br");
+}
+
+
