6414621a169dba330e9f47f40ad7e1a680a33909
Nurkyz-Chynybaeva <nurkyzchynybaeva29@gmail.com>
2021-02-06 22:22:39 +0600

    fixed some mistakes

--- script.js
+++ script.js
@@ -1,89 +1,37 @@
 
 
 for (let i = 1; i <= 10; i++){
-  for (let i2 = 2; i2 <= 2; i2++)
+  for (let i2 = 1; i2 <= 10; i2++)
   {
    
-   document.write("<table border=3 >" + "<tr>" +"<td>" + i2 + " * " + i + " = " + i2 * i + "</td>" + "</tr>" + "</table" );
+   document.write("<table border='3'>" + "<tr>" +"<td>" + i + " * " + i2 + " = " + i2 * i + "</td>" + "</tr>" + "</table>" );
   }
-  document.write("<br");
-}
-
-
-
-for (let i = 1; i <= 10; i++){
-  for (let i2 = 3; i2 <= 3; i2++)
-  {
-   
-   document.write("<table border=3 >" + "<tr>" +"<td>" + i2 + " * " + i + " = " + i2 * i + "</td>" + "</tr>" + "</table" );
-  }
-  document.write("<br");
-}
-
-
-
-for (let i = 1; i <= 10; i++){
-  for (let i2 = 4; i2 <= 4; i2++)
-  {
-   
-   document.write("<table border=3 >" + "<tr>" +"<td>" + i2 + " * " + i + " = " + i2 * i + "</td>" + "</tr>" + "</table" );
-  }
-  document.write("<br");
-}
-
-
-
-for (let i = 1; i <= 10; i++){
-  for (let i2 = 5; i2 <= 5; i2++)
-  {
-   
-   document.write("<table border=3 >" + "<tr>" +"<td>" + i2 + " * " + i + " = " + i2 * i + "</td>" + "</tr>" + "</table" );
-  }
-  document.write("<br");
+  document.write("<br>");
 }
 
 
 
 
-for (let i = 1; i <= 10; i++){
-  for (let i2 = 6; i2 <= 6; i2++)
-  {
-   
-   document.write("<table border=3 >" + "<tr>" +"<td>" + i2 + " * " + i + " = " + i2 * i + "</td>" + "</tr>" + "</table" );
-  }
-  document.write("<br");
-}
 
 
 
-for (let i = 1; i <= 10; i++){
-  for (let i2 = 7; i2 <= 7; i2++)
-  {
-   
-   document.write("<table border=3 >" + "<tr>" +"<td>" + i2 + " * " + i + " = " + i2 * i + "</td>" + "</tr>" + "</table" );
-  }
-  document.write("<br");
-}
 
 
 
-for (let i = 1; i <= 10; i++){
-  for (let i2 = 8; i2 <= 8; i2++)
-  {
-   
-   document.write("<table border=3 >" + "<tr>" +"<td>" + i2 + " * " + i + " = " + i2 * i + "</td>" + "</tr>" + "</table" );
-  }
-  document.write("<br");
-}
 
 
 
-for (let i = 1; i <= 10; i++){
-  for (let i2 = 9; i2 <= 9; i2++)
-  {
-   
-   document.write("<table border=3 >" + "<tr>" +"<td>" + i2 + " * " + i + " = " + i2 * i + "</td>" + "</tr>" + "</table" );
-  }
-  document.write("<br");
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
 
